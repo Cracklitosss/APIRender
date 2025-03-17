d7faea9faf2fd5e10295e88005721b0a0a787bc0
Carlos Marroquin <marroquinca17@gmail.com>
2025-03-17 07:46:50 -0600

    Configurar puerto para Render

--- src/index.js
+++ src/index.js
@@ -167,7 +167,7 @@ app.post('/api/registro', (req, res) => {
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 }); 
